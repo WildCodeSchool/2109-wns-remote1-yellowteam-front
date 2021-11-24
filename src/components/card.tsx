2aaf85abfce9ec729b8df15a2e12bcbcdcbbf2a3
lilykr <lisa-lou.k@hotmail.fr>
2021-11-24 17:29:44 +0100

    added first component card with svg Placeholder

---
+++ src/components/card.tsx
@@ -0,0 +1,25 @@
+import React, { ReactElement } from 'react'
+import { Box, Text } from '@chakra-ui/react'
+
+interface ICard {
+  title: string
+  photo: ReactElement
+  tag: string
+}
+
+const Card = ({ photo, tag, title }: ICard): JSX.Element => (
+  <Box
+    backgroundColor="#FFFFFF"
+    width={218}
+    minHeight={78}
+    borderRadius="10"
+    border="1px solid black"
+    padding="14px 9px 14px 9px"
+  >
+    <Text textStyle="h2">{title}</Text>
+    <Box>{photo}</Box>
+    <Box>{tag}</Box>
+  </Box>
+)
+
+export default Card
