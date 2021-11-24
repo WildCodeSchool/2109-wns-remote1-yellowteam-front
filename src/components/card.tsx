56687337e4968d02bbc7514daf52ab4d68d8c05f
lilykr <lisa-lou.k@hotmail.fr>
2021-11-24 18:24:20 +0100

    component card

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,24 +1,30 @@
 import React, { ReactElement } from 'react'
-import { Box, Text } from '@chakra-ui/react'
+import { Box, Text, Flex } from '@chakra-ui/react'
 
 interface ICard {
   title: string
   photo: ReactElement
-  tag: string
+  tag: ReactElement
 }
 
 const Card = ({ photo, tag, title }: ICard): JSX.Element => (
   <Box
     backgroundColor="#FFFFFF"
-    width={218}
-    minHeight={78}
+    width="218px"
+    minHeight="78px"
     borderRadius="10"
-    border="1px solid black"
-    padding="14px 9px 14px 9px"
+    padding="9px 14px 9px 14px"
   >
     <Text textStyle="h2">{title}</Text>
-    <Box m="12px 4px">{photo}</Box>
-    <Box>{tag}</Box>
+    <Flex
+      alignItems="center"
+      justifyContent="space-between"
+      height="24px"
+      mt="12px"
+    >
+      <Box>{tag}</Box>
+      <Box m="12px 4px">{photo}</Box>
+    </Flex>
   </Box>
 )
 
