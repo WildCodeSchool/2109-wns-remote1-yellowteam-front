bec92e1f1be202d4a9e71f57859464ba403891d8
lilykr <lisa-lou.k@hotmail.fr>
2021-11-24 12:07:20 +0100

    changed location chakra provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,11 @@
-import React, { useEffect, useState } from 'react'
-import { apiUrl } from './settings'
-import './App.css'
+import React from 'react'
+import { ChakraProvider } from '@chakra-ui/react'
 
 function App(): JSX.Element {
-  const [message, setMessage] = useState(null)
-  const [error, setError] = useState<Error | null>(null)
-
-  useEffect(() => {
-    const fetchMessage = async () => {
-      try {
-        const res = await fetch(`${apiUrl}/dummy-endpoint`)
-        if (!res.ok) {
-          throw new Error(res.statusText)
-        }
-        const data = await res.json()
-        setMessage(data.message)
-      } catch (err) {
-        if (err instanceof Error) {
-          setError(err)
-        } else {
-          setError(new Error('Unknown error'))
-        }
-      }
-    }
-
-    fetchMessage()
-  })
   return (
-    <div className="App">
-      <h1>React template</h1>
-      {message && <p>Message received from server: {message}</p>}
-      {error && <p>Error while fetching: {error?.message}</p>}
-    </div>
+    <ChakraProvider>
+      <div className="App">hello world</div>
+    </ChakraProvider>
   )
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { ChakraProvider } from '@chakra-ui/react'
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 
 ReactDOM.render(
   <React.StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 )
