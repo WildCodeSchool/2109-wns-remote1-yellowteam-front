820bf534f2722023afbf4188cf33d32effcaeeac
lilykr <lisa-lou.k@hotmail.fr>
2021-11-25 14:51:59 +0100

    remove card component from app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import { ChakraProvider, Box } from '@chakra-ui/react'
+import { ChakraProvider } from '@chakra-ui/react'
 import mainTheme from './theme/mainTheme'
-import Card from './components/Card'
-import Placeholder from './static/svg/Placeholder'
 import TasksListUser from './pages/TasksListUser'
-import Tag from './components/Tags'
 
 function App(): JSX.Element {
   return (
@@ -14,17 +11,6 @@ function App(): JSX.Element {
         <Routes>
           <Route path="/" element={<TasksListUser />} />
         </Routes>
-        <Card
-          title="je suis un titre"
-          photo={<Placeholder />}
-          tag={
-            <Tag
-              tagColor={mainTheme.colors.lightRed}
-              text="bug"
-              textColor={mainTheme.colors.darkRed}
-            />
-          }
-        />
       </Router>
     </ChakraProvider>
   )
