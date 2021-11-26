6805165679286f635733ff4d1a818f9e414b6415
lilykr <lisa-lou.k@hotmail.fr>
2021-11-26 11:57:15 +0100

    project title changes when selected in user task page

--- src/molecules/UserBackLogHeader.tsx
+++ src/molecules/UserBackLogHeader.tsx
@@ -1,36 +1,45 @@
-import React from 'react'
+/* eslint-disable react/no-array-index-key */
+
+import React, { useState } from 'react'
 import { Flex, Text, Box, Select, Checkbox } from '@chakra-ui/react'
 
 interface IBackLogHeader {
   titleList: string[]
 }
 
-const UserBackLogHeader = ({ titleList }: IBackLogHeader): JSX.Element => (
-  <Box marginLeft="69px">
-    <Flex
-      flexDirection="row"
-      alignItems="center"
-      mb="33px"
-      mr="95px"
-      justifyContent="space-between"
-    >
-      <Text textStyle="h2">TITRE À RECUP DU LOCAL STORAGE?</Text>
-      <Checkbox colorScheme="red" fontFamily="Avenir" fontWeight="800">
-        Assigned only to me
-      </Checkbox>
-    </Flex>
-    <Select
-      fontFamily="Avenir"
-      fontWeight="800"
-      width="320px"
-      bg="white"
-      placeholder="Select a project"
-    >
-      {titleList.map((title) => (
-        <option value="option">{title}</option>
-      ))}
-    </Select>
-  </Box>
-)
+const UserBackLogHeader = ({ titleList }: IBackLogHeader): JSX.Element => {
+  const [titleSelected, setTitleSelected] = useState<string>(titleList[0])
+
+  return (
+    <Box marginLeft="69px">
+      <Flex
+        flexDirection="row"
+        alignItems="center"
+        mb="33px"
+        mr="95px"
+        justifyContent="space-between"
+      >
+        <Text textStyle="h2">{titleSelected}</Text>
+        <Checkbox colorScheme="red" fontFamily="Avenir" fontWeight="800">
+          Assigned only to me
+        </Checkbox>
+      </Flex>
+      <Select
+        fontFamily="Avenir"
+        fontWeight="800"
+        width="320px"
+        bg="white"
+        value={titleSelected}
+        onChange={(event) => setTitleSelected(event.target.value)}
+      >
+        {titleList.map((title, index) => (
+          <option key={index} value={title}>
+            {title}
+          </option>
+        ))}
+      </Select>
+    </Box>
+  )
+}
 
 export default UserBackLogHeader
