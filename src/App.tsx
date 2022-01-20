9c1de3342bb1ab8dcb25929b5e61c43344dcb6ce
JIdayyy <julien_abbadie@hotmail.fr>
2022-01-20 18:50:22 +0100

    fix: 🐛 router merge dev

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,8 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import { ChakraProvider } from '@chakra-ui/react'
 import { ApolloProvider } from '@apollo/client'
-<<<<<<< HEAD
 import Layout from './components/Layout'
 import Login from './pages/Login'
-=======
-import Layout from 'components/Layout'
-import Login from 'pages/Login'
->>>>>>> 6c0e59cff6db1d209fb7989dba0f4dc596643176
 import mainTheme from './theme/mainTheme'
 import apolloClient from './services/graphql'
 import TasksListUser from './pages/TasksListUser'
@@ -20,16 +15,18 @@ function App(): JSX.Element {
   const { isAuth } = useAppState()
 
   return (
-
     <ApolloProvider client={client}>
       <ChakraProvider theme={mainTheme}>
         <Layout>
-            <Routes>
-              <Route path="/login" element={<Login />} />
-            </Routes>
-            <Routes>
-              <Route path="/" element={<TasksListUser />} />
-            </Routes>
+          {isAuth ? (
+            <>
+              <Routes>
+                <Route path="/" element={<TasksListUser />} />
+              </Routes>
+            </>
+          ) : (
+            <Login />
+          )}
         </Layout>
       </ChakraProvider>
     </ApolloProvider>
