4962b1fadae6dcb1e2ae83da82ab0b4e4d6217de
lilykr <lisa-lou.k@hotmail.fr>
2022-02-22 18:19:06 +0100

    wip details project

--- src/pages/ProjectsDetails.tsx
+++ src/pages/ProjectsDetails.tsx
@@ -1,9 +1,8 @@
-import { Box, Spinner, Text } from '@chakra-ui/react'
+import { Box, Spinner, Text, Image } from '@chakra-ui/react'
 import React, { ReactElement } from 'react'
 import { useParams } from 'react-router-dom'
 import WhitePannel from 'src/components/WhitePannel'
 import useProjects from 'src/hooks/useProjects'
-import { Project } from 'src/types/Project'
 
 const ProjectDetails = (): ReactElement => {
   const { projects, loading } = useProjects()
@@ -18,7 +17,14 @@ const ProjectDetails = (): ReactElement => {
 
   const project = projects?.find((p) => p.id === projectId)
 
-  return <WhitePannel>{project?.title}</WhitePannel>
+  return (
+    <WhitePannel>
+      <Text textStyle="h3">{project?.title}</Text>
+      {project?.tasks.map((t) => (
+        <Image src={t.user.avatar} />
+      ))}
+    </WhitePannel>
+  )
 }
 
 export default ProjectDetails
