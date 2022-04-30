68d26c35d8b029b9c0ea3b25b0d8fb1fe81a7f99
JIdayyy <julien_abbadie@hotmail.fr>
2022-04-30 20:05:21 +0200

    :recycle: (navlinks) i did the navigation config object

--- src/components/Layout/UserNavBar.tsx
+++ src/components/Layout/UserNavBar.tsx
@@ -1,25 +1,12 @@
-import React from 'react'
 import { Flex, Box, Switch } from '@chakra-ui/react'
 import { useLocation, useNavigate } from 'react-router-dom'
-import useAppState from 'src/hooks/useAppState'
-import ProjectIcon from 'src/static/svg/ProjectIcon'
-import { useMutateLogoutMutation } from 'src/generated/graphql'
-import TasksIcon from '../../static/svg/TasksIcon'
+import navLinks from 'src/config/navLinks.config'
 import mainTheme from '../../definitions/chakra/theme/mainTheme'
-import UserIcon from '../../static/svg/UserIcon'
-import SignOutIcon from '../../static/svg/SignOutIcon'
+import SignOutButton from '../Assets/SignOutButton'
 
 const UserNavBar = (): JSX.Element => {
   const navigate = useNavigate()
-  const { dispatchLogout } = useAppState()
 
-  const [mutateLogout] = useMutateLogoutMutation({
-    onCompleted: () => dispatchLogout(),
-    // eslint-disable-next-line no-console
-    onError: (err) => console.log('error', err),
-  })
-
-  const setTab = (tab: string) => navigate(`/${tab}`)
   const location = useLocation()
 
   return (
@@ -32,52 +19,22 @@ const UserNavBar = (): JSX.Element => {
       justifyContent="space-evenly"
       z-index="1"
     >
-      <Box
-        as="button"
-        data-testid="tasks-button"
-        onClick={() => setTab('board')}
-      >
-        <TasksIcon
-          color={
-            location.pathname.includes('board')
-              ? mainTheme.colors.orange
-              : mainTheme.colors.deactivatedGrey
-          }
-        />
-      </Box>
-      <Box
-        as="button"
-        data-testid="projects-button"
-        onClick={() => setTab('projects')}
-      >
-        <ProjectIcon
-          color={
-            location.pathname.includes('projects')
-              ? mainTheme.colors.orange
-              : mainTheme.colors.deactivatedGrey
-          }
-        />
-      </Box>
-      <Box
-        as="button"
-        data-testid="user-icon-button"
-        onClick={() => setTab('profile')}
-      >
-        <UserIcon
-          color={
-            location.pathname.includes('profile')
-              ? mainTheme.colors.orange
-              : mainTheme.colors.deactivatedGrey
-          }
-        />
-      </Box>
-      <Box
-        as="button"
-        data-testid="sign-out-button"
-        onClick={() => mutateLogout()}
-      >
-        <SignOutIcon color={mainTheme.colors.deactivatedGrey} />
-      </Box>
+      {navLinks.map((link) => (
+        <Box
+          as="button"
+          data-testid={link['data-testid']}
+          onClick={() => navigate(link.link)}
+        >
+          <link.icon
+            color={
+              location.pathname.includes(link.name)
+                ? mainTheme.colors.orange
+                : mainTheme.colors.deactivatedGrey
+            }
+          />
+        </Box>
+      ))}
+      <SignOutButton />
       <Switch />
     </Flex>
   )
